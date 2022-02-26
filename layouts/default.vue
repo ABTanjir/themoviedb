a173ea9acd45b0451812639600ee8adc85f985c8
ABTanjir <abtanjir@gmail.com>
2022-02-26 20:33:26 +0600

    init: initiated and implemented basic scleton

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="wrapper">
+    <Header />
+      <div class="container">
+        <Nuxt />
+      </div>
+    <Footer />
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <form role="search" method="get" class="search-form form" action="">
+      <label>
+        <span class="screen-reader-text">Search for tv show...</span>
+        <input type="search" class="search-field" placeholder="Type something..." value="" name="s" title="" />
+      </label>
+      <input type="submit" class="search-submit button" value="SEARCH " />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style lang="css">
+
+</style>
+
